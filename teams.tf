505693b0ae316d0b65921db88f33a8acbfc88ac1
Ross Nanopoulos <zythosec@gmail.com>
2019-11-17 12:17:13 -0500

    Adds contributors team and zythosec membership

---
+++ teams.tf
@@ -0,0 +1,9 @@
+resource "github_team" "contributors" {
+  name        = "contributors"
+}
+
+resource "github_team_membership" "some_team_membership" {
+  team_id  = github_team.contributors.id
+  username = "zythosec"
+  role     = "maintainer"
+}
