643d257916112cd26ab32e3bfa7036fbe7b5860c
Ross Nanopoulos <zythosec@gmail.com>
2019-11-17 12:32:26 -0500

    Adds sandbox repository

--- repositories.tf
+++ repositories.tf
@@ -15,3 +15,21 @@ resource "github_team_repository" "tf-github-contributors" {
   repository = github_repository.tf-github.name
   permission = "push"
 }
+
+resource "github_repository" "sandbox" {
+  name               = "sandbox"
+  description        = "Sandbox repository for experiments"
+  private            = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  auto_init          = true
+  gitignore_template = "Python"
+  license_template   = "mit"
+}
+
+resource "github_team_repository" "sandbox-contributors" {
+  team_id    = github_team.contributors.id
+  repository = github_repository.tf-github.name
+  permission = "push"
+}
