8b1850dd43f10cce4b88b43f9023a3385c45cfd2
Ross Nanopoulos <zythosec@gmail.com>
2019-11-18 19:04:02 -0500

    Fixes sandbox-contributors. Adds site repository.

--- repositories.tf
+++ repositories.tf
@@ -30,6 +30,24 @@ resource "github_repository" "sandbox" {
 
 resource "github_team_repository" "sandbox-contributors" {
   team_id    = github_team.contributors.id
-  repository = github_repository.tf-github.name
+  repository = github_repository.sandbox.name
+  permission = "push"
+}
+
+resource "github_repository" "site" {
+  name               = "site"
+  description        = "Code for hackinghockey.com"
+  private            = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  auto_init          = true
+  gitignore_template = "Go"
+  license_template   = "mit"
+}
+
+resource "github_team_repository" "site-contributors" {
+  team_id    = github_team.contributors.id
+  repository = github_repository.site.name
   permission = "push"
 }
